4a73e953b5afe1823f0a1f7dc0e83842038e34a7
Nidhikashyap04 <67714154+Nidhikashyap04@users.noreply.github.com>
2021-05-26 12:58:03 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,57 @@
 #include <iostream>
 #include<queue>
+#include<cmath>
 using namespace std;
 
-void smallestAndSecondSmallest(int *arr,int n)
-{
+void printRunningMedian(int *arr, int n) {
     priority_queue<int,vector<int>,greater<int> > minHeap;
+    priority_queue<int> maxHeap;
+
     for(int i=0;i<n;i++)
     {
-        minHeap.push(arr[i]);
+        if(maxHeap.size() && arr[i]>maxHeap.top())
+        {
+            minHeap.push(arr[i]);
+        }else
+        {
+            maxHeap.push(arr[i]);
+        }
+        if(abs((int)maxHeap.size() - (int)minHeap.size())>1)
+        {
+            if(maxHeap.size()>minHeap.size())
+            {
+                int temp = maxHeap.top();
+                maxHeap.pop();
+                minHeap.push(temp);
+            }else
+            {
+                int temp = minHeap.top();
+                minHeap.pop();
+                maxHeap.push(temp);
+            }
+        }
+        int median ;
+        int totalSize = maxHeap.size() + minHeap.size();
+
+        if(totalSize%2==1)
+        {
+            if(maxHeap.size()>minHeap.size())
+            {
+                median = maxHeap.top();
+            }else
+            {
+                median = minHeap.top();
+            }
+        }
+        else
+        {
+            median =0;
+            if(!maxHeap.empty()) median += maxHeap.top();
+            if(!minHeap.empty()) median += minHeap.top();
+            median/=2;
+        }
+        cout<<median<<" ";
     }
-    int smallest = minHeap.top();
-    minHeap.pop();
-    int secondSmallest = minHeap.top();
-    cout<<"Smallest Element is "<<" "<<smallest<<endl;
-    cout<<"Second Smallest Element is"<<" "<<secondSmallest<<endl;
 }
 
 int main()
@@ -27,7 +65,7 @@ int main()
         cin >> arr[i];
     }
 
-    smallestAndSecondSmallest(arr, n);
+    printRunningMedian(arr, n);
 
     delete[] arr;
 }
